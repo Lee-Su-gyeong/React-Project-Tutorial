a4750c0a45c9968188d09343848b02f472de3ae8
Lee-Su-gyeong <dune2011@naver.com>
2020-11-18 16:14:07 +0900

    Init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo"></img>
+      <h2>같이 개발 해보자!!!</h2>
+    </div>
+  );
+}
+
+export default App;
